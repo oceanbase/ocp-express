536cb735a7e62ef121a9244780fed628c767c80f
dengfuping <fuping.dfp@antgroup.com>
2023-10-14 16:19:20 +0800

    fix(tracert): should set spmAPos, bizType and so on

--- frontend/src/global.ts
+++ frontend/src/global.ts
@@ -17,6 +17,7 @@
  */
 import { setLocale, getLocale } from 'umi';
 import moment from 'moment';
+import tracert from '@/util/tracert';
 
 const validLocalMap = {
   // 英文
@@ -73,3 +74,14 @@ const momentLocaleMap = {
 };
 
 moment.locale(momentLocaleMap[locale] || 'en');
+
+tracert.set({
+  // 埋点 a 位
+  spmAPos: 'a3647',
+  bizType: 'common',
+  // 开启后会监听路由变化，并触发页面访问埋点上报
+  ifRouterNeedPv: true,
+  // 开启曝光上报
+  autoExpo: true,
+  autoLogPv: true,
+});
